a11b8a09a59dd9f547d8c711d15da2c3276dd2ac
1dam <1dam@hz319575.TartangaLH.eus>
2019-02-11 12:34:05 +0100

    Clase completa

--- TestProject/src/prueba/Empleado.java
+++ TestProject/src/prueba/Empleado.java
@@ -1,6 +1,40 @@
 package prueba;
 
+import java.time.LocalDate;
+
 public class Empleado {
 
+	private String mombre;
+	private String apellido;
+	private LocalDate fechaNac;
+	public String getMombre() {
+		return mombre;
+	}
+	public void setMombre(String mombre) {
+		this.mombre = mombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public LocalDate getFechaNac() {
+		return fechaNac;
+	}
+	public void setFechaNac(LocalDate fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+	public Empleado(String mombre, String apellido, LocalDate fechaNac) {
+		super();
+		this.mombre = mombre;
+		this.apellido = apellido;
+		this.fechaNac = fechaNac;
+	}
+	public Empleado() {
+		super();
+	}
+	
+	
 	
 }
